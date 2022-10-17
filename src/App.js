2006236162353af35167c1460cf23f36b7500a51
Rahul Ekbote <rahulek@gmail.com>
2022-10-17 10:57:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the world of ReactJS</h1>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
