af48d5665b63c8fdd738db01afbfee7dfc90b678
Rahul Ekbote <rahulek@gmail.com>
2022-10-20 20:02:26 +0530

    Resuable book form and including it in the Books component

---
+++ src/components/BookForm.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import uuid from "react-uuid";
+import "./BookForm.css";
+
+const BookForm = (props) => {
+  let [title, setTitle] = useState("");
+  let [author, setAuthor] = useState("");
+  let [tags, setTags] = useState("");
+  let [price, setPrice] = useState("");
+
+  //Input Change Handlers
+  const titleChangeHandler = (e) => {
+    console.log(`Title being changed`);
+  };
+
+  const authorChangeHandler = (e) => {
+    //Modify the author state to reflect the user entered keystroke
+    console.log(`Author being changed`);
+  };
+
+  const tagsChangeHandler = (e) => {
+    //Modify the tags state to reflect the user entered keystroke
+    console.log(`Tags being changed`);
+  };
+
+  const priceChangeHandler = (e) => {
+    //Modify the tags state to reflect the user entered keystroke
+    console.log(`Price being changed`);
+  };
+
+  //Form submit handlers
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    //Collect all the information into an object
+    const bookData = {
+      id: uuid(),
+      date: new Date(Date.now()), //now
+      title,
+      author,
+      tags: tags.split(","),
+      price,
+    };
+    console.log(`BookData: ${bookData}`);
+  };
+
+  return (
+    <div className="form_wrapper">
+      <form onSubmit={formSubmitHandler}>
+        <div>
+          <label htmlFor="name">Title </label>
+          <input
+            name="title"
+            type="text"
+            placeholder="Book Title"
+            value={title}
+            onChange={titleChangeHandler}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="name">Author </label>
+          <input
+            name="author"
+            type="text"
+            placeholder="Author"
+            value={author}
+            onChange={authorChangeHandler}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="tags">Tags </label>
+          <input
+            name="tags"
+            type="text"
+            placeholder="Tags separated by commas"
+            value={tags}
+            onChange={tagsChangeHandler}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="name">Price </label>
+          <input
+            name="price"
+            type="number"
+            placeholder="Price in Rupees between 100 and 10,000"
+            min="100"
+            max="10000"
+            value={price}
+            onChange={priceChangeHandler}
+          />
+        </div>
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+};
+
+export default BookForm;
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,20 +1,25 @@
 import "./Books.css";
-import "./BookEntry";
 import BookEntry from "./BookEntry";
+import BookForm from "./BookForm";
 
 const Books = (props) => {
   const books = props.books;
 
-  return books.map((book) => (
-    <BookEntry
-      key={book.id}
-      date={book.date}
-      title={book.title}
-      author={book.author}
-      tags={book.tags}
-      price={book.price}
-    />
-  ));
+  return (
+    <div>
+      <BookForm />
+      {books.map((book) => (
+        <BookEntry
+          key={book.id}
+          date={book.date}
+          title={book.title}
+          author={book.author}
+          tags={book.tags}
+          price={book.price}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Books;
